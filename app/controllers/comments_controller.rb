50be1fa8468886cfab51f2c3c7ef7afde9e214d5
Candice Peters <candice.peters@flatironschool.com>
2021-09-21 13:09:47 -0500

    added to dom started create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,14 @@ class CommentsController < ApplicationController
         comments = Comment.all
         render json: comments
     end
+
+    def create 
+        comment = Comment.new(comment_params)
+    end
+
+    private
+
+    def comment_params 
+        
+    end
 end
